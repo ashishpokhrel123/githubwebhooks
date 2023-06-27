5369c310408624308bd0e7abe810333b2285e55c
aashishesr <aashish@esrtech.io>
2023-06-27 15:21:31 +0545

    chnage

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,27 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Body() payload: any): {status:number, message:string, data: { author:string , commitMessage:string, commitDate:string }} {
+   if (payload && payload.head_commit) {
+      const author = payload.head_commit.author.name;
+      const commitMessage = payload.head_commit.message;
+      const commitDate = payload.head_commit.timestamp;
+
+      return {
+        status: HttpStatus.OK,
+        message: 'Report generated succesfully',
+        data: {
+          author: author || null,
+          commitMessage: commitMessage || null,
+          commitDate: commitDate || null,
+        },
+      };
+    }
+    return {
+      status: HttpStatus.NOT_FOUND,
+      message: 'No data in payload',
+      data:null
+    };
   }
 
   @Get('generatedReport')
