fdc995a4cfce32863782180d36d1e4ffa18d826c
aashishesr <aashish@esrtech.io>
2023-06-27 19:16:14 +0545

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,13 +8,11 @@ interface CommitData {
 
 @Controller()
 export class AppController {
-  private commitHistory: any;
-  
+  private commitHistory: any = null;
 
   @Post()
   handlePostRequest(@Body() payload: any): { status: number; message: string } {
     this.commitHistory = payload;
-     console.log(this.commitHistory, "cm")
     return {
       status: HttpStatus.OK,
       message: 'Payload stored successfully',
@@ -23,21 +21,26 @@ export class AppController {
 
   @Get()
   handleGetRequest(): { status: number; message: string; data: CommitData | null } {
-    console.log(this.commitHistory, "comm")
-
-      const { author, message, timestamp } = this.commitHistory.repository.head_commit;
-
-      const data: CommitData = {
-        authorFullName: author?.name || null,
-        commitMessage: message || null,
-        commitDate: timestamp || null,
-      };
-
+    if (!this.commitHistory || !this.commitHistory.head_commit) {
       return {
-        status: HttpStatus.OK,
-        message: 'Report generated successfully',
-        data,
+        status: HttpStatus.NO_CONTENT,
+        message: 'No commit data available',
+        data: null,
       };
-    
+    }
+
+    const { author, message, timestamp } = this.commitHistory.head_commit;
+
+    const data: CommitData = {
+      authorFullName: author?.name || null,
+      commitMessage: message || null,
+      commitDate: timestamp || null,
+    };
+
+    return {
+      status: HttpStatus.OK,
+      message: 'Report generated successfully',
+      data,
+    };
   }
 }
