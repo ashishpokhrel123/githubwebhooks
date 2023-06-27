cb4351f1e015bf0d252c28d3125d2614c406324e
aashishesr <aashish@esrtech.io>
2023-06-27 20:28:50 +0545

    impleemnt redis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, HttpStatus, Post } from '@nestjs/common';
+import redis from 'redis';
 
 interface CommitData {
   authorFullName: string | null;
@@ -8,62 +9,67 @@ interface CommitData {
 
 @Controller()
 export class AppController {
-  private commitHistory: any[] = [];
+  private redisClient: any;
+
+  constructor() {
+    this.redisClient = redis.createClient();
+  }
 
   @Post()
   handlePostRequest(@Body() payload: any): { status: number; message: string } {
-    this.commitHistory = payload;
+    const commitHistoryKey = 'commitHistory';
+    this.redisClient.rpush(commitHistoryKey, JSON.stringify(payload));
     return {
       status: HttpStatus.OK,
       message: 'Payload stored successfully',
     };
   }
 
-@Get()
-handleGetRequest(): {
-  status: number;
-  message: string;
-  data: CommitData[] | null;
-} {
-  if (this.commitHistory.length === 0) {
-    return {
-      status: HttpStatus.NO_CONTENT,
-      message: 'No commits yet',
-      data: null,
-    };
+  private async getCommitHistoryFromRedis(key: string): Promise<string[]> {
+    return new Promise<string[]>((resolve, reject) => {
+      this.redisClient.lrange(key, 0, -1, (error, commitList) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(commitList);
+        }
+      });
+    });
   }
 
-  const commitDataList: CommitData[] = this.commitHistory.map(commit => {
-    const { author, message, timestamp } = commit.head_commit;
+  @Get()
+  async handleGetRequest(): Promise<{
+    status: number;
+    message: string;
+    data: CommitData[] | null;
+  }> {
+    const commitHistoryKey = 'commitHistory';
+    try {
+      const commits = await this.getCommitHistoryFromRedis(commitHistoryKey);
 
-    return {
-      authorFullName: author?.name || null,
-      commitMessage: message || null,
-      commitDate: timestamp || null,
-    };
-  });
+      if (!commits || commits.length === 0) {
+        return {
+          status: HttpStatus.NO_CONTENT,
+          message: 'No commits available',
+          data: null,
+        };
+      }
 
-  return {
-    status: HttpStatus.OK,
-    message: 'Commits retrieved successfully',
-    data: commitDataList,
-  };
-}
+      const parsedCommitsHistory: CommitData[] = commits.map((commit: string) =>
+        JSON.parse(commit),
+      );
 
-  @Get('/commits')
-  getAllCommits(): { status: number; message: string; data: any[] } {
-    if (this.commitHistory.length === 0) {
       return {
-        status: HttpStatus.NO_CONTENT,
-        message: 'No commit data available',
-        data: [],
+        status: HttpStatus.OK,
+        message: 'Commit data retrieved successfully from Redis',
+        data: parsedCommitsHistory,
+      };
+    } catch (error) {
+      return {
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Error retrieving commit data from Redis',
+        data: null,
       };
     }
-
-    return {
-      status: HttpStatus.OK,
-      message: 'All commits retrieved successfully',
-      data: this.commitHistory,
-    };
   }
 }
