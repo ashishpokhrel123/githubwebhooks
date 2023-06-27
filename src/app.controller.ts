64a115558fe952570b6cd3d67834c7e4b166f5b2
aashishesr <aashish@esrtech.io>
2023-06-27 21:02:06 +0545

    hhh

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,58 +12,37 @@ export class AppController {
 
   @Post()
   handlePostRequest(@Body() payload: any): { status: number; message: string } {
-    this.commitHistory = payload;
+    this.commitHistory.push(payload);
     return {
       status: HttpStatus.OK,
       message: 'Payload stored successfully',
     };
   }
 
-@Get()
-handleGetRequest(): {
-  status: number;
-  message: string;
-  data: CommitData[] | null;
-} {
-  if (this.commitHistory.length === 0) {
-    return {
-      status: HttpStatus.NO_CONTENT,
-      message: 'No commits yet, please deploy',
-      data: null,
-    };
-  }
-
-  const commitDataList: CommitData[] = this.commitHistory.map(commit => {
-    const { author, message, timestamp } = commit.head_commit;
-
-    return {
-      authorFullName: author?.name || null,
-      commitMessage: message || null,
-      commitDate: timestamp || null,
-    };
-  });
-
-  return {
-    status: HttpStatus.OK,
-    message: 'Commits retrieved successfully',
-    data: commitDataList,
-  };
-}
-
-  @Get('/commits')
-  getAllCommits(): { status: number; message: string; data: any[] } {
+  @Get()
+  handleGetRequest(): { status: number; message: string; data: CommitData[] | null } {
     if (this.commitHistory.length === 0) {
       return {
         status: HttpStatus.NO_CONTENT,
         message: 'No commit data available',
-        data: [],
+        data: null,
       };
     }
+
+    const commitDataList: CommitData[] = this.commitHistory.map(commit => {
+      const { author, message, timestamp } = commit.head_commit;
+
+      return {
+        authorFullName: author?.name || null,
+        commitMessage: message || null,
+        commitDate: timestamp || null,
+      };
+    });
 
     return {
       status: HttpStatus.OK,
       message: 'All commits retrieved successfully',
-      data: this.commitHistory,
+      data: commitDataList,
     };
   }
 }
