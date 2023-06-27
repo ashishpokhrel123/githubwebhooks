0a668d81d03c4a65630f736dd399965c85f22499
aashishesr <aashish@esrtech.io>
2023-06-27 16:23:47 +0545

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,8 @@ export class AppController {
   getHello(@Body() payload: any): { status: number; message: string; data: any } {
     if (payload && payload.repository && payload.commits && payload.head_commit) {
       const author = payload.sender.login;
-      const commitMessage = payload.head_commit.message;
-      const commitDate = payload.head_commit.timestamp;
+      const commitMessage = payload.commits.message;
+      const commitDate = payload.commits.timestamp;
 
       return {
         status: HttpStatus.OK,
